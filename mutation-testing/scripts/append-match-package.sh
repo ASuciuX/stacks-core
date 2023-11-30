717f2ce8a58c08d64ad48c64907aff180131f68a
ASuciuX <asuciu@hiro.so>
2023-12-01 01:03:43 +0200

    feat: append match package v1

--- mutation-testing/scripts/append-match-package.sh
+++ mutation-testing/scripts/append-match-package.sh
@@ -31,7 +31,6 @@ escape_for_awk() {
 # Iterate over the specified files
 for file in "${FILES[@]}"; do
     pr_file="$PR_FOLDER/$file"
-    stable_file="$STABLE_FOLDER_PARENT/$file"
 
     echo "Processing file: $file"
 
@@ -41,47 +40,33 @@ for file in "${FILES[@]}"; do
         while IFS= read -r line; do
             echo "Reading line from PR file: $line"
 
-            # Extract the core pattern without the line number and escape it for awk
+            # Extract the package from which the line is coming from
+            local_package=${$line%%/*}
+
+            # Extract the after the number line without the line number and escape it for awk
             core_pattern=$(echo "$line" | sed -E 's/^[^:]+:[0-9]+:(.+)/\1/')
             escaped_pattern=$(escape_for_awk "$core_pattern")
             echo "Extracted and escaped pattern: $escaped_pattern"
 
-            # Iterate over each file in the STABLE folder
+            # Iterate over each file in the STABLE folder combined with local_package
             for target_file in "${FILES[@]}"; do
-                target_path="$STABLE_FOLDER_PARENT/$target_file"
+                target_path="$STABLE_FOLDER_PARENT/$local_package/$target_file"
                 echo "Checking against STABLE file: $target_path"
 
                 # Remove the line matching the pattern, ignoring line numbers
                 awk -v pat="$escaped_pattern" '$0 !~ pat' "$target_path" > temp_file && mv temp_file "$target_path"
             done
+
+            # Append PR line to the corresponding package and file
+            echo "$line" >> "$STABLE_FOLDER_PARENT/$local_package/$file"
+
         done < "$pr_file"
     else
         echo "PR file $pr_file is empty or does not exist, skipping..."
     fi
 done
 
-# After processing all lines, append contents from PR_DIR to STABLE_DIR
-for file in "${FILES[@]}"; do
-    cat "$PR_FOLDER/$file" >> "$STABLE_FOLDER_PARENT/$file"
-done
-
-# Echo the contents of the STABLE folder for verification
-echo "Updated contents of STABLE folder:"
-for file in "${FILES[@]}"; do
-    stable_file="$STABLE_FOLDER_PARENT/$file"
-    echo "Contents of $stable_file:"
-    cat "$stable_file"
-    echo ""
-done
-
 echo "Script completed."
-
-
-
-
-# goes only through txt
-
-
 
 
 # remove temp/mutants.out
