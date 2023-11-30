88ae8f5e734e74671f50096734c6998748896647
ASuciuX <asuciu@hiro.so>
2023-11-30 02:55:44 +0200

    feat: remove duplicated line output

--- mutating-script/script.sh
+++ mutating-script/script.sh
@@ -17,31 +17,33 @@ find . -type d -empty -delete
 
 ### Append from PR_DIR to STABLE_DIR
 
-# # Directories
-# STABLE_DIR="./../mutants.out.old"
+# Directories
+STABLE_DIR="./../mutants.out.old"
 
-# # Array of file names
-# FILES=("caught.txt" "missed.txt" "timeout.txt" "unviable.txt")
+# Array of file names
+FILES=("caught.txt" "missed.txt" "timeout.txt" "unviable.txt")
 
-# # Function to remove a line from a file
-# remove_line() {
-#     local line="$1"
-#     local file="$2"
-#     # Use grep to filter out the line and save it back to the file
-#     grep -Fv "$line" "$file" > temp.txt && mv temp.txt "$file"
-# }
+# Function to remove a line from a file
+# fails if the removed line is the only one in the file
+remove_line() {
+    local line="$1"
+    local file="$2"
+    echo "$file $line"
+    grep -Fvx "$line" "$file" > temp.txt && mv temp.txt "$file"
+}
 
-# # Process each file
-# for file in "${FILES[@]}"; do
-#     while IFS= read -r line; do
-#         # Check and remove the line from any file in STABLE_DIR if it exists
-#         for check_file in "${FILES[@]}"; do
-#             if grep -Fxq "$line" "$STABLE_DIR/$check_file"; then
-#                 remove_line "$line" "$STABLE_DIR/$check_file"
-#             fi
-#         done
-#     done < "$PR_DIR/$file"
-# done
+# Process each file
+for file in "${FILES[@]}"; do
+    while IFS= read -r line; do
+        # Check and remove the line from any file in TARGET_DIR if it exists
+        for check_file in "${FILES[@]}"; do
+            if grep -Fxq -- "$line" "$STABLE_DIR/$check_file"; then
+                echo "file: $STABLE_DIR/$check_file"
+                remove_line "$line" "$STABLE_DIR/$check_file"
+            fi
+        done
+    done < "$PR_DIR/$file"
+done
 
 # # After processing all lines, append contents from PR_DIR to STABLE_DIR
 # for file in "${FILES[@]}"; do
