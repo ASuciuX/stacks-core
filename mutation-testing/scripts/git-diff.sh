131d0ce5ddf718e858efbca292505bb1dd34f416
ASuciuX <asuciu@hiro.so>
2023-12-05 14:02:36 +0200

    feat: fixed script errors, writing the git diff to file

--- mutation-testing/scripts/git-diff.sh
+++ mutation-testing/scripts/git-diff.sh
@@ -20,7 +20,7 @@ cd mutation-testing
 
 # get the differences since the last commit
 last_commit_hash=$(<./packages-output/last_commit_hash.txt)
-git diff "$last_commit_hash"
+git diff "$last_commit_hash" > git.diff
 
 # it runs cargo mutants for those specific changed functions and outputs to /temp/mutants.out
 # for faster builds: increase number to 4 if at least 16 gb ram and 6 cores CPU
@@ -30,11 +30,11 @@ cargo mutants --no-shuffle -j 2 -vV --in-diff git.diff --output temp/
 cd scripts
 
 # call append-match-package.sh to update the content from the stable output
-sh append-match-package.sh
+bash append-match-package.sh
 
 # print the modified mutants for testing, to be deleted
 echo "Files ending with .txt in ./temp/mutants.out:"
-for file in ./temp/mutants.out/*.txt; do
+for file in ../temp/mutants.out/*.txt; do
     if [ -f "$file" ]; then
         echo "File: $file"
         echo "Contents:"
